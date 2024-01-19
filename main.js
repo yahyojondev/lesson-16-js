7b3683bb30374ada5c75414ac37f38d62465f4e6
yahyojondev <nabijonovdeveloper@gmail.com>
2024-01-19 10:03:27 +0500

    simple

---
+++ main.js
@@ -0,0 +1,55 @@
+// class 1
+
+// class Rectangle {
+//     #width;
+//     #height;
+//     constructor(width,height){
+//         this.#width = width;
+//         this.#height = height
+//     }
+//     getWidth(){
+//         return this.#width;
+//     }
+    
+//     getHeight(){
+//         return this.#height;
+//     }
+//     setSides(a,b){
+//         this.#width = a;
+//         this.#height = b;
+//     }
+//     get perimetr(){
+//         return 2*(this.#width + this.#height);
+//     }
+//     get area(){
+//         return (this.#width * this.#height)
+//     }
+// }
+
+// let r1 = new Rectangle(10,20);
+// r1.setSides(30,40)
+// console.log(r1.getWidth());
+// console.log(r1.getHeight());
+
+// console.log(r1.perimetr);
+// console.log(r1.area);
+
+// class 2
+
+// let obj = {
+//     a : 1,
+//     b : 2,
+//     c : 3,
+// }
+
+// // console.log(Object.keys(obj));
+
+// Object.customKeys = function(obj){
+//     let res = [];
+//     for(key in obj){
+//         res.push(key);
+//     }
+//     return res ;
+// }
+
+// console.log(Object.customKeys(obj ));
